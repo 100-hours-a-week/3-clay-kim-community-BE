ec5c818911544c123582a93467f05f99b6712b42
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2025-10-19 22:58:33 +0900

    init: community 초기화

---
+++ week5-community-front/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// public 폴더 안의 정적 파일 전부 제공
+app.use(express.static(path.join(__dirname, "public")));
+
+// 루트 요청 → index.html 열기
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/pages/index/index.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/pages/login/login.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Frontend running at http://localhost:3000");
+});
