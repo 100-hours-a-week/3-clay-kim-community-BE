9d43669a9a547b896a68de90c678c46806cb61c9
Seonjun Kim <kimsunjun1221@gmail.com>
2025-12-07 22:47:57 +0900

    feat: 이미지 추가로 게시글 수정기능 추가

--- src/main/java/kr/kakaotech/community/global/config/RedisConfig.java
+++ src/main/java/kr/kakaotech/community/global/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package kr.kakaotech.community.global;
+package kr.kakaotech.community.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
--- src/main/java/kr/kakaotech/community/repository/PostRepository.java
+++ src/main/java/kr/kakaotech/community/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package kr.kakaotech.community.repository;
 
-import kr.kakaotech.community.dto.response.PostDetailResponse;
 import kr.kakaotech.community.dto.response.PostSummaryResponse;
+import kr.kakaotech.community.dto.response.PostSummaryWithImageResponse;
 import kr.kakaotech.community.entity.Post;
 import kr.kakaotech.community.entity.PostType;
 import org.springframework.data.domain.Pageable;
@@ -63,6 +63,7 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
                 join post_statuses ps on ps.post = p
                 join users u on p.user = u
                 where p.deleted = false
+                AND p.type = 'completed'
                 order by ps.likeCount desc
             """)
     List<PostSummaryResponse> findTop10Post(Pageable pageable);
@@ -81,16 +82,44 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
     List<PostSummaryResponse> findPostByNickname(String nickname, Pageable pageable);
 
     @Query("""
-        SELECT new kr.kakaotech.community.dto.response.PostDetailResponse(
-                p.title, p.content, p.createdAt,
-                u.id, p.nickname, u.image.url, p.type
-            )
+        SELECT p
         FROM posts p
-        JOIN users u ON p.user = u
+        LEFT JOIN FETCH p.postImages pi
+        LEFT JOIN FETCH pi.image
+        JOIN FETCH p.user u
+        JOIN FETCH u.image
         WHERE p.deleted = false
         AND p.id = :postId
     """)
-    Optional<PostDetailResponse> findPostDetails(int postId);
+    Optional<Post> findPostDetailsWithImages(int postId);
+
+    @Query("""
+    SELECT new kr.kakaotech.community.dto.response.PostSummaryWithImageResponse(
+        p.id,
+        p.title,
+        p.nickname,
+        p.createdAt,
+        ps.likeCount,
+        ps.commentCount,
+        ps.viewCount,
+        u.image.url,
+        p.type,
+        (
+            SELECT MIN(pi2.image.url)
+            FROM post_image pi2
+            WHERE pi2.post = p
+        )
+    )
+    FROM posts p
+        JOIN post_statuses ps ON ps.post = p
+        JOIN users u ON p.user = u
+    WHERE p.deleted = false
+    ORDER BY p.createdAt DESC
+    """)
+    List<PostSummaryWithImageResponse> findPostWithImage(Pageable pageable);
+
 
     int countPostByType(PostType type);
+
+    int countByDeletedFalseAndType(PostType type);
 }
