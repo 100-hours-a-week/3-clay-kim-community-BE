4fafc101906bd51908406c9508d2365d883945bb
Seonjun Kim <kimsunjun1221@gmail.com>
2026-02-17 01:32:31 +0900

    ♻️ refactor: 이미지 저장소를 조건부 빈으로 분기하고 S3 인증을 AWS 기본 체인으로 전환

--- src/main/java/kr/kakaotech/community/global/config/S3Config.java
+++ src/main/java/kr/kakaotech/community/global/config/S3Config.java
@@ -2,11 +2,13 @@ package kr.kakaotech.community.global.config;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(name = "image.storage.type", havingValue = "s3")
 public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
