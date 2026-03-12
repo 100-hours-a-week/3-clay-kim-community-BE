600c69e57b443e1f3eda08f31fd60f2edb625ad5
Seonjun Kim <kimsunjun1221@gmail.com>
2026-03-13 00:57:50 +0900

    ✨ feat: validate 추가

--- src/main/java/kr/kakaotech/community/dto/request/CommentRequest.java
+++ src/main/java/kr/kakaotech/community/dto/request/CommentRequest.java
@@ -1,8 +1,12 @@
 package kr.kakaotech.community.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CommentRequest {
+    @NotBlank(message = "댓글 내용을 입력해주세요.")
+    @Size(max = 500, message = "댓글은 500자 이하로 작성해주세요.")
     private String content;
 }
--- src/main/java/kr/kakaotech/community/dto/request/UserLoginRequest.java
+++ src/main/java/kr/kakaotech/community/dto/request/UserLoginRequest.java
@@ -1,5 +1,7 @@
 package kr.kakaotech.community.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserLoginRequest {
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
